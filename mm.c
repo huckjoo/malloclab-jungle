8676ff8229e7e22fc7ea5dae6011ea192b925905
huckjoo <huckjoo96@naver.com>
2021-12-13 10:09:28 +0000

    memlib.c model init

--- mm.c
+++ mm.c
@@ -24,16 +24,45 @@
  ********************************************************/
 team_t team = {
     /* Team name */
-    "ateam",
+    "team 8",
     /* First member's full name */
-    "Harry Bovik",
+    "Kwon HyukJoo",
     /* First member's email address */
-    "bovik@cs.cmu.edu",
+    "omp0109@naver.com",
     /* Second member's full name (leave blank if none) */
     "",
     /* Second member's email address (leave blank if none) */
     ""
 };
+// Basic constants and macros
+#define WSIZE 4 // 워드 = 헤더 = 풋터 사이즈(bytes)
+#define DSIZE 8 // 더블워드 사이즈(bytes)
+#define CHUNKSIZE (1<<12) // heap을 이정도 늘린다(bytes)
+
+#define MAX(x, y) ((x) > (y)? (x):(y))
+
+// pack a size and allocated bit into a word 
+#define PACK(size, alloc) ((size) | (alloc))
+
+// Read and wirte a word at address p
+//p는 (void*)포인터이며, 이것은 직접 역참조할 수 없다.
+#define GET(p)     (*(unsigned int *)(p)) 
+#define PUT(p,val) (*(unsigned int *)(p) = (val)) //-------? 뭐하는애지
+
+// Read the size and allocated fields from address p 
+#define GET_SIZE(p)  (GET(p) & ~0x7) // ~0x00000111 -> 0x11111000(얘와 and연산하면 size나옴)
+#define GET_ALLOC(p) (GET(p) & 0x1)
+
+// Given block ptr bp, compute address of its header and footer
+#define HDRP(bp) ((char *)(bp) - WSIZE)
+#define FTRP(bp) ((char *)(bp) + GET_SIZE(HDRP(bp))-DSIZE) //헤더+데이터+풋터 -(헤더+풋터)
+
+// Given block ptr bp, compute address of next and previous blocks
+// 현재 bp에서 WSIZE를 빼서 header를 가리키게 하고, header에서 get size를 한다.
+// 그럼 현재 블록 크기를 return하고(헤더+데이터+풋터), 그걸 현재 bp에 더하면 next_bp나옴
+#define NEXT_BLKP(bp) ((char *)(bp) + GET_SIZE(((char *)(bp) - WSIZE)))
+#define PREV_BLKP(bp) ((char *)(bp) + GET_SIZE(((char *)(bp) - DSIZE)))
+
 
 /* single word (4) or double word (8) alignment */
 #define ALIGNMENT 8
@@ -47,9 +76,45 @@ team_t team = {
 /* 
  * mm_init - initialize the malloc package.
  */
+static char *heap_listp = mem_start_brk + 2 // heap의 첫 번째 주소 + 2
 int mm_init(void)
 {
+    //Create the initial empty heap
+    if((heap_listp = mem_sbrk(4*WSIZE)) == (void *)-1) //heap_listp는 무슨 pointer임 ----------------?
+        return -1;                              // 불러올 수 없으면 -1 return  
+    PUT(heap_listp, 0);                         // Alignment padding
+    PUT(heap_listp + (1*WSIZE), PACK(DSIZE,1)); // P.H 8(크기)/1(할당됨)
+    PUT(heap_listp + (2*WSIZE), PACK(DSIZE,1)); // P.F 8/1
+    PUT(heap_listp + (3*WSIZE), PACK(0,1));     // E.H(헤더로만 구성) 0/1
+    heap_listp += (2*WSIZE); //이건 뭐야--------------------?
+    // Extend the empty heap with a free block of CHUNKSIZE bytes
+    if (extend_heap(CHUNKSIZE/WSIZE) == NULL) //word가 몇개인지 확인해서 넣으려고
+        return -1;
     return 0;
+}
+// heap을 CHUNKSIZE byte로 확장하고 초기 가용 블록을 생성한다.
+// 여기까지 진행되면 할당기는 초기화를 완료하고, application으로부터
+// 할당과 반환 요청을 받을 준비를 완료하였다.
+// extend_heap은 두 가지 경우에 호출된다.
+// 1. heap이 초기화될 때
+// 2. mm_malloc이 적당한 맞춤(fit)을 찾지 못했을 때
+static void *extend_heap(size_t words)
+{
+    char *bp;
+    size_t size;
+
+    // Allocate an even number of words to maintain alignment
+    size = (words % 2) ? (words + 1) * WSIZE : words * WSIZE;
+    if ((long)(bp = mem_sbrk(size)) == -1) //size를 불러올 수 없으면
+        return NULL;
+    
+    // Initialize free block header/footer and the epilogue header
+    PUT(HDRP(bp), PACK(size,0)); // Free block header
+    PUT(FTRP(bp), PACK(size,0)); // Free block footer
+    PUT(HDRP(NEXT_BLKP(bp)), PACK(0,1)); // New epilogue header
+
+    // Coalesce(연결후 합침) if the previous block was free
+    return coalesce(bp);
 }
 
 /* 
