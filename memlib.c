d3dbd742a2929c4fed930460af86f0370d34f33d
huckjoo <huckjoo96@naver.com>
2021-12-13 12:51:01 +0000

    Implicit - first fit

--- memlib.c
+++ memlib.c
@@ -16,29 +16,22 @@
 
 /* private variables */
 static char *mem_start_brk;  /* points to first byte of heap */
-static char *mem_brk;        /* points to last byte of heap plus 1? */
-static char *mem_max_addr;   /* largest legal heap address plus 1 */ 
-// mem_heap과 mem_brk 사이의 바이트들은 할당된 가상메모리를 나타냄
-// mem_brk 다음에 오는 바이트들은 미할당 가상메모리를 나타냄
+static char *mem_brk;        /* points to last byte of heap */
+static char *mem_max_addr;   /* largest legal heap address */ 
+
 /* 
  * mem_init - initialize the memory system model
- * 힙에 사용가능한 가상메모리를 더블워드로 정렬된 바이트의 배열로 모델한 것을 시작
  */
 void mem_init(void)
 {
-    //밑에 중복되는데 뭐가맞는것인가 -----------------------?
-    mem_heap = (char *)Malloc(MAX_HEAP);
-    mem_brk = (char *)mem_heap;
-    mem_max_addr = (char *)(mem_heap + MAX_HEAP);
-
     /* allocate the storage we will use to model the available VM */
     if ((mem_start_brk = (char *)malloc(MAX_HEAP)) == NULL) {
 	fprintf(stderr, "mem_init_vm: malloc error\n");
 	exit(1);
     }
-    // 일단 책대로 한다
-    // mem_max_addr = mem_start_brk + MAX_HEAP;  /* max legal heap address */
-    // mem_brk = mem_start_brk;                  /* heap is empty initially */
+
+    mem_max_addr = mem_start_brk + MAX_HEAP;  /* max legal heap address */
+    mem_brk = mem_start_brk;                  /* heap is empty initially */
 }
 
 /* 
@@ -58,15 +51,15 @@ void mem_reset_brk()
 }
 
 /* 
- * mem_sbrk - simple model of the sbrk function. Extends the heap(추가적인 heap 메모리 요청)
- *    by incr bytes and returns the start address of the new area. 
- *    In this model, the heap cannot be shrunk.
+ * mem_sbrk - simple model of the sbrk function. Extends the heap 
+ *    by incr bytes and returns the start address of the new area. In
+ *    this model, the heap cannot be shrunk.
  */
 void *mem_sbrk(int incr) 
 {
     char *old_brk = mem_brk;
 
-    if ( (incr < 0) || ((mem_brk + incr) > mem_max_addr)) { //heap 축소 거부, maxheap까지만 할당가능
+    if ( (incr < 0) || ((mem_brk + incr) > mem_max_addr)) {
 	errno = ENOMEM;
 	fprintf(stderr, "ERROR: mem_sbrk failed. Ran out of memory...\n");
 	return (void *)-1;
